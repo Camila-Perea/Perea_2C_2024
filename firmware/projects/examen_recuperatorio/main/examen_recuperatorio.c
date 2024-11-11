ebc0ae4fba2d40a02768e1c11f2cf56debdc8dc3
Camila-Perea <camila.perea[200~>
2024-11-11 16:19:47 -0300

    subo carpeta donde se realizara el recuperatorio del examen

---
+++ firmware/projects/examen_recuperatorio/main/examen_recuperatorio.c
@@ -0,0 +1,127 @@
+/*! @mainpage Template
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 12/09/2023 | Document creation		                         |
+ *
+ * @author Camila Perea (camila.perea@ingenieria.uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include <led.h>
+#include "hc_sr04.h"
+#include "timer_mcu.h"
+#include "uart_mcu.h"
+#include "analog_io_mcu.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+/*==================[macros and definitions]=================================*/
+/** Periodo de la interrupción del temporizador de la medición. */
+#define CONFIG_BLINK_PERIOD 100000
+TaskHandle_t medir_task_handle = NULL;
+
+/*==================[internal data definition]===============================*/
+/** Variable que indica si la medición de distancia está activada. */
+bool on = true;
+/** Variable que almacena el valor que mide de distancia.*/
+uint16_t distancia;
+/** Variable que almacena la velocidad calculada.*/
+float velocidad;
+/**Me guarda el valor de voltaje que me da la balanza*/
+uint16_t valor_sensado;
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+
+void FuncTimerDistancia(void* param){
+
+    vTaskNotifyGiveFromISR(medir_task_handle, pdFALSE); 
+}
+
+void Pesar()
+{
+	AnalogInputReadSingle(CH1, &valor_sensado);
+
+	UartSendString(UART_PC, "Peso: \r\n");
+	UartSendString(UART_PC, "Velocidad: \r\n");
+
+
+}
+
+void CalcularVelodidad()
+{
+	velocidad=distancia/CONFIG_BLINK_PERIOD;
+	if (velocidad>3)
+	{
+		LedOn(LED_1);
+		LedOff(LED_2);
+		LedOff(LED_3);
+	}
+	else if(velocidad>=0 && distancia<8)
+	{ 
+		LedOff(LED_1);
+		LedOn(LED_2);
+		LedOff(LED_3);
+	}
+	else if(velocidad=0)
+	{ 
+		LedOff(LED_1);
+		LedOff(LED_2);
+		LedOn(LED_3);
+		Pesar();
+	}
+}
+
+void MedirDistancia(void *pvParameter)
+{
+	while(true) 
+	{
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		distancia=HcSr04ReadDistanceInCentimeters();
+			
+		if(distancia<100)
+		{
+			CalcularVelocidad();
+		}
+
+			
+	}
+}
+
+void app_main(void){
+	
+	// Configuración de temporizador
+    timer_config_t timer_medir_distancia = {
+        .timer = TIMER_A,
+        .period = CONFIG_BLINK_PERIOD,
+        .func_p = FuncTimerDistancia,
+        .param_p = NULL
+    };
+
+    TimerInit(&timer_medir_distancia);
+	xTaskCreate(&MedirDistancia, "distancia_task_handle", 2048, NULL, 5, NULL);
+
+    TimerStart(timer_medir_distancia.timer);
+
+
+}
+/*==================[end of file]============================================*/
